0604ce4717979f328fc9bae26d601f90aac749c2
Mehedi1952 <159076048+abdullahalafjal@users.noreply.github.com>
2026-03-03 03:11:00 +0600

    added to do card with api

---
+++ todo.js
@@ -0,0 +1,28 @@
+const loadTodo = () => {
+  url = "https://jsonplaceholder.typicode.com/users/1/todos";
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      displayTodo(data);
+    });
+};
+
+const displayTodo = (todos) => {
+  const todoContainer = document.getElementById("todo-container");
+  todoContainer.innerHTML = "";
+
+  todos.forEach((todo) => {    console.log(todo);
+
+    const todoCard = document.createElement("div");
+    todoCard.innerHTML = `
+            <div class="todo-card">
+            <p>${todo.completed == true ? '<i class="fa-regular fa-square-check"></i>' : '<i class="fa-solid fa-square-check"></i>'}  </p> 
+            <h3>${todo.title}</h3>
+            <p>Completed: ${todo.completed}</p>
+            </div>
+        `;
+    todoContainer.appendChild(todoCard);
+  });
+};
+
+loadTodo();
